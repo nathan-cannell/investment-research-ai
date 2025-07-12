a34d6fa2e578689d8d0e45cfc1395cd1a500d73e
ncannell <ncannell@uw.edu>
2025-07-12 11:08:32 -0700

    SPY returns calculation

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,21 +1,26 @@
 import requests
 from polygon import RESTClient
+import pandas as pd
 
 def OHLCV(API_KEY, ticker, from_date, to_date):
     url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{from_date}/{to_date}?apiKey={API_KEY}"
-
     response = requests.get(url)
     data = response.json()
 
+    bars = []
     for bar in data.get("results", []):
-        print({
-            "date": bar["t"],
+        bars.append({
+            "timestamp": bar["t"],
             "open": bar["o"],
             "high": bar["h"],
             "low": bar["l"],
             "close": bar["c"],
             "volume": bar["v"]
         })
+    df = pd.DataFrame(bars)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    return df
+
 
 def base_returns(API_KEY, ticker, from_date, to_date):
     client = RESTClient(API_KEY)
@@ -29,7 +34,30 @@ def base_returns(API_KEY, ticker, from_date, to_date):
         limit=50000
     ):
         aggs.append(a)
-    return aggs
+        # Convert to DataFrame
+    data = [{
+        'timestamp': a.timestamp,
+        'open': a.open,
+        'high': a.high,
+        'low': a.low,
+        'close': a.close,
+        'volume': a.volume,
+        'vwap': a.vwap,
+        'transactions': a.transactions
+    } for a in aggs]
+    
+    df = pd.DataFrame(data)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df.set_index('timestamp', inplace=True)
+
+
+    # Assuming you have a DataFrame 'spy_df' with 'date' and 'close' columns
+    df['return'] = df['close'].pct_change()
+    
+    # Resample to quarterly and calculate compounded return
+    df_quarterly = (1 + df['return']).resample('Q').prod() - 1
+    df_quarterly = df_quarterly.rename('spy_quarterly_return')
+    return df, df_quarterly
 
 def future_excess_return(portfolio_return, benchmark_return):
     excess_return = portfolio_return - benchmark_return
@@ -38,13 +66,19 @@ def future_excess_return(portfolio_return, benchmark_return):
 def main():
     print("Data Pipeline Initialized")
     API_KEY = "TTXLaHBArYCOrGJpkuNaMJdVeJ0dLDRG"
-    ticker = "AAPL"
     from_date = "2025-01-01"
     to_date = "2025-07-10"
 
-    url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{from_date}/{to_date}?apiKey={API_KEY}"
+    # excess_return = future_excess_return(0.10, 0.03)
+    apple_vals = OHLCV(API_KEY, "AAPL", from_date, to_date)
+    bench_returns, spy_quarterly = base_returns(API_KEY, "SPY", from_date, to_date)
 
-    excess_return = future_excess_return(0.10, 0.03)
-    OHLCV(API_KEY, ticker, from_date, to_date)
-    bench_returns = base_returns(API_KEY, "I:SPX", from_date, to_date)
-    print(bench_returns)
+
+
+    print(apple_vals.head())
+    print(bench_returns.head())
+    print(spy_quarterly.head())
+
+
+if __name__ == "__main__":
+    main()
